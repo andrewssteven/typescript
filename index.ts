baa105c301dc9987db54a459a7e11909132c69f6
andrewssteven <andrewssteven151@gmail.com>
2025-06-07 20:28:39 +0100

    create: created a vehicle inteface that in a car class to return engine started

---
+++ index.ts
@@ -0,0 +1,26 @@
+interface vehicle {
+    make: string,
+    model: string,
+    year: number,
+    start: () => void
+}
+
+class Car implements vehicle {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  start(): void {
+    console.log("Car engine started");
+  }
+}
+
+const myCar = new Car("Toyota", "Corolla", 2022);
+
+myCar.start();
